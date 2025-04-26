8b241d40944411dd652c49c987870ce77b7b7db5
Pedro Fernandez <pedro@pandadesigners.com>
2025-04-26 17:56:33 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+
+const App = () => {
+  return (
+    <div className='text-6xl text-indigo-400'>App</div>
+  )
+}
+
+export default App
---
+++ util/util.ts
@@ -0,0 +1,6 @@
+export class Utils {
+    private constructor() { }
+    static formatCode(text: string) {
+        return text.split(' ')
+    }
+}
